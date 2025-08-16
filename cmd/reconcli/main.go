55f4f4b219b2aa7a5f69818da24f3704694a6fa1
situmorangbastian <bastianpaskal@gmail.com>
2025-08-16 10:23:31 +0700

    feat: add application entry point

---
+++ cmd/reconcli/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/situmorangbastian/recon-cli/internal/reader"
+	"github.com/situmorangbastian/recon-cli/internal/reconcile"
+	"github.com/situmorangbastian/recon-cli/internal/service"
+)
+
+var (
+	sysTxnDateTimeLayout = []string{
+		"2006-01-02 15:04:05",
+	}
+	bankStmtDateLayout = []string{
+		"2006-01-02",
+	}
+)
+
+func main() {
+	flag.Usage = func() {
+		progName := filepath.Base(os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", progName)
+		flag.PrintDefaults()
+	}
+
+	systemTxnPath := flag.String("systempath", "", "path file to system transactions (required)")
+	bankStmtPath := flag.String("bankstmtpath", "", "comma-separated path files to bank statement (required)")
+	startDate := flag.String("startdate", "", "startdate in YYYY-MM-DD format (required)")
+	endDate := flag.String("enddate", "", "enddate in YYYY-MM-DD format (required)")
+
+	flag.Parse()
+
+	switch true {
+	case *startDate == "":
+		fmt.Println("Error: -startdate required")
+		flag.Usage()
+		os.Exit(1)
+	case *endDate == "":
+		fmt.Println("Error: -enddate required")
+		flag.Usage()
+		os.Exit(1)
+	case *systemTxnPath == "":
+		fmt.Println("Error: -systempath required")
+		flag.Usage()
+		os.Exit(1)
+	case *bankStmtPath == "":
+		fmt.Println("Error: -bankstmtpath required")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	bankStmtPaths := strings.Split(*bankStmtPath, ",")
+	reader := reader.NewReader(sysTxnDateTimeLayout, bankStmtDateLayout, bankStmtPaths, *systemTxnPath)
+	svc := service.NewService(reader)
+	reconcile.New(svc)
+
+	// TODO: Add reconcile result
+}
